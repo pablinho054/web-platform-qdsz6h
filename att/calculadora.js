115ead58babca3a95f012c2d08a98019fb9834c7
pablinho054 <pnr@aluno.ifnmg.edu.br>
2022-07-14 08:45:36 -0300

    3 files added, 3 deleted

--- att/calculadora.js
+++ att/calculadora.js
@@ -8,5 +8,4 @@ function media() {
   res.innerHTML = `<p>Calculando a média final de <mark>${nom}</mark>.</p>`
   res.innerHTML += `<p>As notas obtidas foram <mark>${n1} e ${n2}</mark>.</p>` 
   res.innerHTML += `<p>A média final será <mark>${med}</mark>.</p>`
-  res.innerHTML += `<p>A mensagem que temos é: <strong style='color:red;'>${msg}</strong></p>` // Note que eu usei até um pouco de CSS pra fazer a mensagem ficar vermelha ;)
-}
+  
